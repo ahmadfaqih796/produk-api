95c5f8e82ae62c629db311e24ccecaa26e5861c9
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2022-11-08 21:08:15 +0700

    commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const aplikasi = express();
+
+let karyawan = [
+  {
+    name: "ahmad",
+    umur: 12,
+  },
+  {
+    name: "faqih",
+    umur: 14,
+  },
+];
+// [] => data objek
+// {} => data array
+aplikasi.get("/", function (request, response) {
+  response.json({ pesan: "data berhasil di ambil", data: karyawan });
+});
+
+aplikasi.listen(3002);
